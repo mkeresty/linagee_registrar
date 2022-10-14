2d0e1e7afea14c1f70f5b26cae939ace95f658f3
mkjuice <masonk@udel.edu>
2022-10-13 23:08:31 -0400

    what changes

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/history":["static\u002Fchunks\u002Fpages\u002Fhistory.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fhistory"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
